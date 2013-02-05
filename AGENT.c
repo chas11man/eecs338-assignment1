307256fa9f142900c1a5e0739d1785cdaa9e6f04
chas11man <chas11man@gmail.com>
2013-02-04 20:38:56 -0500

    Fixing fork and smoking functions

--- AGENT.c
+++ AGENT.c
@@ -2,28 +2,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-main()
-{
-	initializeSuplies();
-
-	printInfo();
-
-	printf("AGENT IS DONE!");
-	exit();
-}
-
-forkSmokers()
+void forkSmokers()
 {
 	printf("FORKING THREE SMOKERS!\n");
-	fork();
-	printf("SMOKER S1:%d\n", gitpid());
+	pid_t pid = fork();
+	if (pid == 0) {
+		//forked
+		printf("SMOKER S1:%d\n", gitpid());
+		smoke();
+	} else (pid < 0) {
+		//failed
+	} else {
+		//agent
+		//dec 1 from each, because consumed by s1.
+		//fork s2 here
+		//repeat
+	}
 	fork();
 	printf("SMOKER S2:%d\n", gitpid());
 	fork();
 	printf("SMOKER S3:%d\n", gitpid());
 }
 
-printInfo()
+void smoke()
+{
+	checkSupplies();
+	listAvaliable();
+	printf("SMOKING!\n");
+}
+
+void checkSupplies()
+{
+	if (getenv("PAPER") == 0) {
+		printf("NO PAPER!\n");
+		exit();
+	} else (getenv("TOBACCO") == 0) {
+		printf("NO TOBACCO!\n");
+		exit();
+	} else (getenv("MATCHES") == 0) {
+		printf("NO MATCHES!\n");
+		exit();
+	}
+}
+
+void listAvaliable()
+{
+	printf("PAPER IS AVAILABLE! P:%d\n", getenv("PAPER"));
+	printf("TOBACCO IS AVAILABLE! T:%d\n", getenv("TOBACCO"));
+	printf("MATCHES ARE AVAILABLE! M:%d\n", getenv("MATCHES"));
+}
+
+void printInfo()
 {
 	printf("AGENT INFO:\n");
 	printf("HOSTNAME:%d\n", gethostname());
@@ -41,9 +70,19 @@ printInfo()
 	printf("PAPER:%d, TOBACCO:%d, MATCHES:%d\n", PAPER, TOBACCO, MATCHES);
 }
 
-initializeSuplies()
+void initializeSuplies()
 {
-	putenv("PAPER=1");
-	putenv("TOBACCO=0");
+	putenv("PAPER=2");
+	putenv("TOBACCO=2");
 	putenv("MATCHES=2");
+}
+
+void main()
+{
+	initializeSuplies();
+
+	printInfo();
+
+	printf("AGENT IS DONE!");
+	exit();
 }
